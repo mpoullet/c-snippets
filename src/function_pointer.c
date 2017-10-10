73a275c980cf732ff668fa8431beef70ddae2f0a
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-10-10 10:58:08 +0200

    Add function pointer example.

---
+++ src/function_pointer.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+
+#define UNUSED(x) (void)(x)
+
+void *get_pointer()
+{
+    return NULL;
+}
+
+int main(int argc, char* argv[])
+{
+    UNUSED(argc);
+    UNUSED(argv);
+
+    void *(*function_pointer_1)();
+    void *(*function_pointer_2)();
+
+    /* warning: ISO C forbids assignment between function pointer and ‘void *’ [-Wpedantic] */
+    function_pointer_1 = get_pointer();
+    /* no warning: as seen in Kerrisk's The Linux Programming Interface: */
+    /* 42.1.3 Obtaining the Address of a Symbol dlsym() pp. 865 */
+    *(void **) (&function_pointer_2) = get_pointer();
+}
+
